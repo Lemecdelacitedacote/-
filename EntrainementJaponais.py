f171f21c95f622a4711ce0433aaecdf60a59da67
Manuel <manuel.pereira@edu.esiee.fr>
2020-11-08 14:14:13 +0100

    premier commit de test

---
+++ EntrainementJaponais.py
@@ -0,0 +1,28 @@
+import random
+
+def init():
+    #Adjectifs en i
+    adji = dict([
+        ("おおきい", "grand"),
+        ("ちいさい", "petit"),
+
+
+    ])
+
+return adji
+
+[, "ながい", "みじかい", "あつい", "さむい", "つめたい", "たかい", "ひくい", "たかい", "やすい", "むずかしい", "やさしい", "あたらしい", "ふるい", "おもしろい", "つまらたい", "おもい", "かるい", "はやい", "おそい", "おいしい", "まずい", "いい", "わるい", "いそがしい"]
+[, "long", "court", "chaud (climat)", "chaud (objet)", "froid (climat)", "frais (objet)", "haut", "bas", "cher", "bon marché", "difficile", "facile", "nouveau/neuf", "vieux/ancien", "intéressant", "ennuyeux", "lourd", "léger", "rapite/tôt", "long/tard", "délicieux/bon", "mauvais/fade", "bon (général)", "mauvais (général)", "occupé" ]
+
+
+
+def main():
+
+
+
+    return None
+
+
+if __name__ == '__main__':
+    main()
+    
